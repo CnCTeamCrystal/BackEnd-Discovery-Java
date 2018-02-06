47bc4b27c328083482a750e571927156aa0b9d61
Saeyoung Kim <heehouse1@gmail.com>
2018-02-06 15:05:20 +0900

    First Commit

---
+++ src/main/java/com/skcc/teamcrystal/discovery/TeamcrystalDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.skcc.teamcrystal.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcrystalDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcrystalDiscoveryApplication.class, args);
+	}
+}
